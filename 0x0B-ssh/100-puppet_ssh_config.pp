dd305fedf3ab9961c0a20b28f90d7a1a8a7654d8
AlpagaSauvage <3645@holbertonschool.com>
2022-04-17 15:05:47 +0000

    add task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# Client configuration file (w/ Puppet)
+file_line { 'PasswordAuthentication':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+
+file_line { 'IdentityFile':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
