c915248aa490973ab8bc3b1795ba424ee45a405c
AlpagaSauvage <90853574+AlpagaSauvage@users.noreply.github.com>
2022-07-22 15:20:43 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Create a file
-file { 'create file':
-  ensure  => present,
-  path    => '/tmp/school',
-  mode    => '0744',
-  group   => 'www-data',
-  owner   => 'www-data',
-  content => 'I love Puppet',
-}
